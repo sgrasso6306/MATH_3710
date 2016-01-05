d9dd682476e491040cf9bcc5aae05493b5fcc313
Steven <Steven@10.0.0.19>
2016-01-05 15:18:59 -0500

    Added basic functionality to SynapseGene

--- MATH3710_ANN/src/ann/neat/SynapseGene.java
+++ MATH3710_ANN/src/ann/neat/SynapseGene.java
@@ -3,6 +3,30 @@ package ann.neat;
 public class SynapseGene {
 	private int _innovationNumber;
 	private boolean _enabled;
+	private int _sourceIndex;
+	private int _destIndex;
+	private double _weight;
 	
+	public SynapseGene(int innovationNumber, int sourceIndex, int destIndex, double weight, boolean enabled) {
+		_innovationNumber = innovationNumber;
+		_sourceIndex = sourceIndex;
+		_destIndex = destIndex;
+		_weight = weight;
+		_enabled = enabled;
+	}
+	
+	public void setWeight(double newWeight) {
+		_weight = newWeight;
+	}
+	public void setEnabled(boolean enabled) {
+		_enabled = enabled;
+	}
+	
+	public double getWeight() {
+		return _weight;
+	}
+	public boolean getEnabled() {
+		return _enabled;
+	}
 	
 }
